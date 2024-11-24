0ca5ced70cb152e0c9fece52957e29c0e1d3b08a
Guillermo Robles <grobles64@ieszaidinvergeles.org>
2024-11-24 13:56:08 +0100

    Fetch con axios

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Movie } from './src/Movie';
-import { Result } from './src/dataMovies';
+import { MoviesResponse, Result } from './src/dataMovies';
 import { movieMapper } from './src/movieMapper';
+import axios from 'axios';
 const url_base = "https://api.themoviedb.org/3/movie/now_playing";
 const api_key = "c76ed6d50b96d2bfc0920abaeade0be3";
 
-(async function datos() {
-  const data = await fetch(`${url_base}?api_key=${api_key}`);
-  const jsonData = await data.json();
-  const movies: Movie[] = jsonData.results.map((item: Result) => movieMapper(item));
+(async function () {
+  const response = await axios.get<MoviesResponse>(`${url_base}?api_key=${api_key}`)
+  const movies: Movie[] = response.data.results.map((item: Result) => movieMapper(item));
   console.log(movies);
 })();
 
