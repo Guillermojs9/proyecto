a5b05b6f7403e09e95dbc7dd4e01363ee99bea00
Guillermo Robles <grobles64@ieszaidinvergeles.org>
2024-11-24 13:30:51 +0100

    Primer fetch

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=c76ed6d50b96d2bfc0920abaeade0be3')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
 export default function App() {
   return (
     <View style={styles.container}>
